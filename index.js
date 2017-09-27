19c418b6de4110dbe4c7e1d7204707449a596815
Garry Clerge <clergeg@gmail.com>
2017-09-27 14:04:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+
+function nestedTarget(){
+  
+  return document.getElementById(nested).querySelector('.target')
+}
+
+
+function deepestChild(){
+
+}
+
+
+function increaseRankBy(n){
+  
+}
